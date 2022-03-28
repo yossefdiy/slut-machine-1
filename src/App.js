a0af26e2cf342f78bd217f09b0c2d6a7b6414b84
diy <yossefdiy@gmail.com>
2022-03-28 18:20:55 +0300

    slot h1

--- src/App.js
+++ src/App.js
@@ -1,50 +1,27 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React from 'react'
+import { useSpring, animated} from 'react-spring'
+import Slot from './slot'
+import Slot2 from './slot'
+import Slot3 from './slot'
+import './App.css'
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
+export default function App() {
+  return (
+    
+    <div className='app'>
+        <div>slot machina***</div>
+        <div className='grid'>
+        <div><Slot/></div>
+        <div><Slot3/></div>
+        <div><Slot2/></div>
+       
+       
+        </div>
+        
+        
+        
+     
+    </div>
+    
+  )
 }
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
